c5f9ed3fabc309865e2ec882aa682dc32aa14a28
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2023-12-18 17:42:33 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Descrizione:
+//chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//va applicato uno sconto del 20% per i minorenni
+//va applicato uno sconto del 40% per gli over 65.
+//L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+// Descrizione:
+
+/*
+    1- Dichiarare una variabile kilometri
+    2- Chiedere i kilometri fatti all'utente
+    3- Salvarlo in una variabile
+
+    4- Dichiarare una variabile età
+    5- Chiedere l'età all'utente
+    6- Salvarlo in una variabile 
+
+    7- Dichiarare una variabile prezzo totale, (0.21 € al km)
+
+    8- applicare sconto 20%, se <18
+    9- applicare sconto 40%, se >65
+    10- non applicare sconto se non si rientra tra 0-18 e 65-99
+
+    11- vedere solo due decimali
+
+    12-Recuperare l'elemento HTML
+    13-Modificare il contenuto
+*/
+
+
